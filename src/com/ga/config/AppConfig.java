c77982ea12af41c6a9981b6fa01b0ce621a87d11
Mohammad Javed <ghazanfar9131@gmail.com>
2019-10-14 16:24:49 -0400

    - Add properties into AppConfig -

--- src/com/ga/config/AppConfig.java
+++ src/com/ga/config/AppConfig.java
@@ -2,11 +2,13 @@ package com.ga.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.ga")
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 
 }
