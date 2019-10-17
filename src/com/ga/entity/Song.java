e26bb051d90b359fc5aee704c90056c97b1237c4
Mohammad Javed <ghazanfar9131@gmail.com>
2019-10-17 12:19:41 -0400

    - fixed bugs

--- src/com/ga/entity/Song.java
+++ src/com/ga/entity/Song.java
@@ -22,7 +22,7 @@ public class Song {
             cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
     @JoinTable(name = "user_song",
             joinColumns = {@JoinColumn(name = "song_id")},
-            inverseJoinColumns = {@JoinColumn(name = "song_id")}
+            inverseJoinColumns = {@JoinColumn(name = "user_id")}
     )
     private List<User> users;
 
